3ca7441e1241a228cb8784a8439006816ea6f0f3
Haj B <phajib@gmail.com>
2019-03-13 18:07:12 -0700

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,25 @@ require 'pry'
 class String
 
   def sentence?
-    
+    include? "."
   end
 
   def question?
-
+    include? "?"
   end
 
   def exclamation?
-
+    include? "!"
   end
 
   def count_sentences
+    #self.split((/\.|\?|\!/).delete_if {|w| w.size < 2}).size
+    #include? (/[^\.!?]+[\.!?]/) #.map(&:strip).count)
+    # self.count
 
+    # Splits sentences and counts them
+    self.scan(/[^\.!?]+[\.!?]/).map(&:strip).count
+    # Caret inside of a character class [^ ] is the negation operator. Which means we are looking for characters which are not present in list: ., ! and ?. + is a greedy operator that returns matches between 1 and unlimited times. (capturing our sentences here and ignoring repetitions like ...) [\.!?] matching characters ., ! or ?.
+    # In a nutshell, we are capturing all characters that are not ., ! or ? till we get characters that are ., ! or ?. Which basically can be treated as a sentence (in broad senses).
   end
 end
